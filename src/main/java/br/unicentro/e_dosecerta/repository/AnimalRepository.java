b02898866803a307addbdf7bb89ca9c517e9cab1
EnViKeyy <pierre__jp@hotmail.com>
2018-07-01 18:47:33 -0300

    Adicionado query para selecionar animal pelo RG

--- src/main/java/br/unicentro/e_dosecerta/repository/AnimalRepository.java
+++ src/main/java/br/unicentro/e_dosecerta/repository/AnimalRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface AnimalRepository extends CrudRepository<Animal, String> {
 
-    @Query("select a from Animal a where  a.animalId = :animalId")
-    public Animal findByAnimalId(@Param("animalId") Integer animalId);
+    @Query("select a from Animal a where  a.rg = :rg")
+    public Animal findByRg(@Param("rg") String rg);
 }
