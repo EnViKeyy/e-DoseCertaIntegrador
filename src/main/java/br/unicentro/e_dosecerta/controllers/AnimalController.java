2b87a35b38da9c0bc103b79cef6626eef481d493
EnViKeyy <pierre__jp@hotmail.com>
2018-07-01 18:45:25 -0300

    Implementação da alteração de animais

--- src/main/java/br/unicentro/e_dosecerta/controllers/AnimalController.java
+++ src/main/java/br/unicentro/e_dosecerta/controllers/AnimalController.java
@@ -11,8 +11,10 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -52,19 +54,50 @@ public class AnimalController {
         Iterable<Especie> especies = especieRpt.findAll();
         Iterable<Animal> animais = animalRpt.findAll();
         List<AnimalEspecie> animalEsps = new ArrayList<>();
-        
-        for(Animal animal : animais) {
+
+        for (Animal animal : animais) {
             animalEsp = new AnimalEspecie();
+            animalEsp.setAnimalId(animal.getAnimalId());
             animalEsp.setNome(animal.getNome());
             animalEsp.setRg(animal.getRg());
-            for(Especie especie : especies) {
-                if(especie.getEspecieId() == animal.getEspecieId())
-                     animalEsp.setEspecie(especie.getNome());
+            for (Especie especie : especies) {
+                if (especie.getEspecieId() == animal.getEspecieId()) {
+                    animalEsp.setEspecie(especie.getNome());
+                }
             }
-        animalEsps.add(animalEsp);
+            animalEsps.add(animalEsp);
         }
-        
+
         mv.addObject("animalEsps", animalEsps);
         return mv;
+    }
+
+    @RequestMapping(value = "animal/{rg}", method = RequestMethod.GET)
+    public ModelAndView detalhesAnimal(@PathVariable("rg") String rg) {
+        Animal animalFind = animalRpt.findByRg(rg);
+        ModelAndView mv = new ModelAndView("alterar/animal");
+
+        AnimalEspecie animalEsp;
+
+        Iterable<Especie> especies = especieRpt.findAll();
+        Iterable<Animal> animais = animalRpt.findAll();
+
+        mv.addObject("especies", especies);
+        mv.addObject("animal", animalFind);
+
+        return mv;
+    }
+
+    @RequestMapping(value = "animal/{rg}", method = RequestMethod.POST)
+    public String alterarAnimal(@RequestParam("rg") String rg, Animal animal, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            attributes.addFlashAttribute("mensagem", "Campos obrigatórios não preenchidos!");
+            return "redirect:/cadastro/animal";
+        }
+        Animal animalFind = animalRpt.findByRg(rg);
+        animal.setAnimalId(animalFind.getAnimalId());
+        animalRpt.save(animal);
+        attributes.addFlashAttribute("mensagem", "Cadastro efetuado com sucesso!");
+        return "redirect:/consulta/animal";
     }
 }
