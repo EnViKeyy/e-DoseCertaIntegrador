64e15c30d6ed97d944cf3defbc952ab3e7b59b92
EnViKeyy <pierre__jp@hotmail.com>
2018-07-01 16:27:47 -0300

    Adição de query para buscar doságem pelo ID do veterinário

--- src/main/java/br/unicentro/e_dosecerta/repository/DosagemRepository.java
+++ src/main/java/br/unicentro/e_dosecerta/repository/DosagemRepository.java
@@ -1,9 +1,12 @@
 package br.unicentro.e_dosecerta.repository;
 
 import br.unicentro.e_dosecerta.models.Dosagem;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface DosagemRepository extends CrudRepository<Dosagem, String> {
 
-    public Dosagem findByDosagemId(Integer dosagemId);
+    @Query("select d from Dosagem d where d.veterinarioId = :veterinarioId")
+    public Dosagem findByVeterinarioId(@Param("veterinarioId") Integer veterinarioId);
 }
