c265e408ef716b1513ccbf2747f57924d2c3bdbf
EnViKeyy <pierre__jp@hotmail.com>
2018-07-03 17:42:50 -0300

    Adicionado mensagem de validação

--- src/main/java/br/unicentro/e_dosecerta/controllers/DosagemController.java
+++ src/main/java/br/unicentro/e_dosecerta/controllers/DosagemController.java
@@ -14,11 +14,14 @@ import br.unicentro.e_dosecerta.util.DosagemFarmacoAnimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class DosagemController {
@@ -37,7 +40,7 @@ public class DosagemController {
 
     @RequestMapping(value = "/cadastro/dosagem", method = RequestMethod.GET)
     private ModelAndView animais() {
-        ModelAndView mv = new ModelAndView("Cadastro/dosagem");
+        ModelAndView mv = new ModelAndView("cadastro/dosagem");
 
         Iterable<Animal> animais = animalRpt.findAll();
         mv.addObject("animais", animais);
@@ -49,7 +52,12 @@ public class DosagemController {
     }
 
     @RequestMapping(value = "/cadastro/dosagem", method = RequestMethod.POST)
-    private String form(Dosagem dosagem) {
+    private String form(@Valid Dosagem dosagem, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            attributes.addFlashAttribute("mensagem", "Campos obrigatórios não preenchidos!");
+            return "redirect:/cadastro/dosagem";
+        }
+        
         Animal animal = animalRpt.findByAnimalId(dosagem.getAnimalId());
         Farmaco farmaco = farmacoRpt.findByFarmacoId(dosagem.getFarmacoId());
 
@@ -91,6 +99,7 @@ public class DosagemController {
         dose2.setPeso(dosagem.getPeso());
         dose2.setVeterinarioId(dosagem.getVeterinarioId());
         dosagemRpt.save(dose2);
+        attributes.addFlashAttribute("mensagem", "Cadastro efetuado com sucesso!");
 
         return "redirect:/cadastro/dosagem";
     }
