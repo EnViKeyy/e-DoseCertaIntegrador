e1bc166d5ef8e01ccab9f1c5dc365d7b4c8dac69
EnViKeyy <pierre__jp@hotmail.com>
2018-07-04 00:57:48 -0300

    Correção de query

--- src/main/java/br/unicentro/e_dosecerta/repository/EspecieRepository.java
+++ src/main/java/br/unicentro/e_dosecerta/repository/EspecieRepository.java
@@ -1,9 +1,12 @@
 package br.unicentro.e_dosecerta.repository;
 
 import br.unicentro.e_dosecerta.models.Especie;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface EspecieRepository extends CrudRepository<Especie, String> {
 
-    public Especie findByEspecieId(Integer especieId);
+    @Query("select e from Especie e where e.especieId = :especieId")
+    public Especie findByEspecieId(@Param("especieId") Integer especieId);
 }
